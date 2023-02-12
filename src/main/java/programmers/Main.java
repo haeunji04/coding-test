c8d3ac4c7b0e1e1cce00a9e4c019e96cbdbd1f4b
haeunji04 <ppippu12@gmail.com>
2023-02-12 23:52:50 +0900

    다른 풀이

---
+++ src/main/java/programmers/Main.java
@@ -0,0 +1,19 @@
+package main.java.programmers;
+
+import main.java.programmers.level0.중복된숫자개수;
+
+/**
+ * 확인용
+ */
+public class Main {
+
+    public static void main(String[] args){
+//        int[] array = {1, 2, 2, 3, 6, 9};
+//        int n = 3;
+
+        중복된숫자개수 result = new 중복된숫자개수();
+//        System.out.println("=======" + result.solution1(array, n));
+
+    }
+
+}
