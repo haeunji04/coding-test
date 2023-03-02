27af0787d5f8948bb2db671f2d2213d0ad66d8da
haeunji04 <ppippu12@gmail.com>
2023-03-03 00:00:51 +0900

    문자열 계산하기 풀이

--- src/main/java/programmers/level0/문자열계산하기.java
+++ src/main/java/programmers/level0/문자열계산하기.java
@@ -12,7 +12,7 @@ public class 문자열계산하기 {
         int answer = 0;
 
         for(int i=0; i<arr.length; i++){
-            if(answer == 0){
+            if(i == 0){
                 answer = Integer.parseInt(arr[i]);
             }
             switch(arr[i]){
